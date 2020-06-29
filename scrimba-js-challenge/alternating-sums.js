2faed5eb99c8bbdcf5a70726d5924d532f293dbf
sairina <sairina.merinotsui@gmail.com>
2020-06-28 23:49:56 -0700

    refactored ternary

--- scrimba-js-challenge/alternating-sums.js
+++ scrimba-js-challenge/alternating-sums.js
@@ -1,12 +1,14 @@
 function alternatingSums(array) {
-  let even = 0; 
+  let even = 0;
   let odd = 0;
   for (let i = 0; i < array.length; i++) {
-      if (i % 2 === 0) {
-          even += array[i];
-      } else {
-          odd += array[i];
-      }
+    // if (i % 2 === 0) {
+    //     even += array[i];
+    // } else {
+    //     odd += array[i];
+    // }
+    i % 2 === 0 ? even += array[i] : odd += array[i];
+
   }
   return [even, odd];
 }
@@ -18,16 +20,16 @@ function alternatingSums(array) {
 */
 describe('alternatingSums()', () => {
   it('returns alternating sums of even and odd', () => {
-      // arrange
-      const nums = [50, 60, 60, 45, 70];
-      
-      // act
-      const result = alternatingSums(nums);
+    // arrange
+    const nums = [50, 60, 60, 45, 70];
 
-      // log
-      console.log("result: ", result);
-      
-      // assert
-      expect(result).toEqual([180, 105]);
+    // act
+    const result = alternatingSums(nums);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toEqual([180, 105]);
   });
 });
