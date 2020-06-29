1d26cb90129369eb0cd26a3a31722653c920650f
sairina <sairina.merinotsui@gmail.com>
2020-06-28 22:32:58 -0700

    added code

---
+++ scrimba-js-challenge/all-the-longest-strings.js
@@ -0,0 +1,41 @@
+function allLongestStrings(array) {
+  //create dictionary holding all the strings of a particular length
+  let longest = {};
+  for (let i = 0; i < array.length; i++) {
+    let elementLength = array[i].length;
+
+    if (longest[elementLength] !== undefined) {
+      longest[elementLength].push(array[i]);
+    } else {
+      longest[elementLength] = new Array();
+      longest[elementLength].push(array[i]);
+    }
+  }
+
+  //iterate over keys in longest to find highest key
+  let highestKey = Object.keys(longest).sort((a, b) => b - a)[0];
+
+  //return the values in that key
+  return longest[highestKey];
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('allLongestStrings()', () => {
+  it('returns all longest strings', () => {
+    // arrange
+    const strings = ["aba", "aa", "ad", "vcd", "aba", "a"];
+
+    // act
+    const result = allLongestStrings(strings);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toEqual(["aba", "vcd", "aba"]);
+  });
+});
