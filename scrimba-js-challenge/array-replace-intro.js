75d030d2632b433223cd240668862d3aca3e95fc
sairina <sairina.merinotsui@gmail.com>
2020-06-28 23:50:52 -0700

    added code

---
+++ scrimba-js-challenge/array-replace-intro.js
@@ -0,0 +1,36 @@
+function arrayReplace(array, elemToReplace, substitutionElem) {
+  //iterate through array
+      //if element to replace is === substitution element
+          //element at that index is reassigned to subelement
+  //return array
+  
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] === elemToReplace) {
+          array[i] = substitutionElem;
+      }
+  }
+  return array;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('arrayReplace()', () => {
+  it('adds a border around entire application', () => {
+      // arrange
+      const array = [1, 2, 1];
+      const elemToReplace = 1;
+      const substitutionElem = 3;
+      
+      // act
+      const result = arrayReplace(array, elemToReplace, substitutionElem);
+
+      // log
+      console.log("result: ", result);
+      
+      // assert
+      expect(result).toEqual([3, 2, 3]);
+  });
+});
