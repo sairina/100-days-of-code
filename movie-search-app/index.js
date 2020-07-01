cd3ccaafa168eff19760728547531658781de6bc
sairina <sairina.merinotsui@gmail.com>
2020-06-30 22:39:13 -0700

    added code

---
+++ movie-search-app/index.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div className='container'>
+      <h1 className='title'>React Movie Search</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
