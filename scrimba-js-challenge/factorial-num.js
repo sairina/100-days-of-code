077a7eaeb5df737bea0876f79e7e331a4be41108
sairina <sairina.merinotsui@gmail.com>
2020-07-04 22:50:50 -0700

    added code and recursive code

---
+++ scrimba-js-challenge/factorial-num.js
@@ -0,0 +1,33 @@
+function factorialNumber(num) {
+  // let product = 1;
+  
+  // for(let i = 1; i <= num; i++) {
+  //     product *= i;
+  // }
+  
+  // return product;
+  
+  if (num <= 1) return 1;
+  return num * factorialNumber(num - 1);
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('factorialNumber()', () => {
+  it('returns factorial of number param', () => {
+      // arrange
+      const num = 5;
+      
+      // act
+      const result = factorialNumber(num);
+
+      // log
+      console.log("result: ", result);
+      
+      // assert
+      expect(result).toBe(120);
+  });
+});
