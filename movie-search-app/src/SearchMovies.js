a16651153afb3a6bab8fe3e388cac425e2927b27
sairina <sairina.merinotsui@gmail.com>
2020-07-06 22:10:26 -0700

    added search movies function

--- movie-search-app/src/SearchMovies.js
+++ movie-search-app/src/SearchMovies.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './SearchMovies.css';
 
 export default function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const query = 'Jurassic Park';
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=34d2cc8126a9b2a930962264a0c490bf&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const response = await fetch(url);
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={searchMovies}>
       <label className='label' htmlFor='query'>Movie Title</label>
-      <input className='input' type='text' name='query' placeholder='e.g. Jurassic Park'/>
+      <input className='input' type='text' name='query' placeholder='e.g. Jurassic Park' />
       <button className='button' type='submit'>Click Me</button>
     </form>
   );
