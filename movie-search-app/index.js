96827caf3bbf12fd3e6157c383b9ebc58394f901
sairina <sairina.merinotsui@gmail.com>
2020-07-06 21:34:02 -0700

    initial commit

--- movie-search-app/index.js
+++
@@ -1,9 +0,0 @@
-function Main() {
-  return (
-    <div className='container'>
-      <h1 className='title'>React Movie Search</h1>
-    </div>
-  );
-}
-
-ReactDOM.render(<Main />, document.getElementById('root'));
