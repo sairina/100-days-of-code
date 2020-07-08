0f74d763136600bddc523c4aff7fb65d5ebf2986
sairina <sairina.merinotsui@gmail.com>
2020-07-07 19:21:52 -0700

    added axios

--- movie-search-app/src/SearchMovies.js
+++ movie-search-app/src/SearchMovies.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './SearchMovies.css';
+import axios from 'axios';
 
 export default function SearchMovies() {
   const searchMovies = async (e) => {
     e.preventDefault();
-
     const query = 'Jurassic Park';
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=34d2cc8126a9b2a930962264a0c490bf&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
-      const response = await fetch(url);
-      const data = response.json();
+      const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=34d2cc8126a9b2a930962264a0c490bf&language=en-US&query=${query}&page=1&include_adult=false`);
+      const data = response.data;
       console.log(data);
     } catch (error) {
       console.log(error);
