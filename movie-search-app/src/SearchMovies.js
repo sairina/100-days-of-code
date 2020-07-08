a55088da5b3bb3a240375c340cb50cc1f6558961
sairina <sairina.merinotsui@gmail.com>
2020-07-07 22:17:32 -0700

    moved MovieCard component out

--- movie-search-app/src/SearchMovies.js
+++ movie-search-app/src/SearchMovies.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './SearchMovies.css';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default function SearchMovies() {
 
@@ -33,22 +34,13 @@ export default function SearchMovies() {
         />
         <button className='button' type='submit'>Search</button>
       </form>
-
       <div className='Card-list'>
         {movies.filter(movie => movie.poster_path).map(movie => (
-          <div className='Card' key={movie.id}>
-            <img className='Card--image' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-              alt={movie.title + ' poster'}
-            />
-            <div className='Card-content'>
-              <h3 className='Card--title'>{movie.title}</h3>
-              <p><small>RELEASE DATE: {movie.release_date}</small></p>
-              <p><small>RATING: {movie.vote_average}</small></p>
-              <p className='Card-desc'>{movie.overview}</p>
-            </div>
-          </div>
+          <MovieCard movie={movie} key={movie.id} />
         ))}
+
       </div>
+
     </>
   );
 }
