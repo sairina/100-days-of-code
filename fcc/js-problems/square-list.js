e1be06ba7c4e0b0ba4ead717695b74ab485012a8
sairina <sairina.merinotsui@gmail.com>
2020-07-11 13:00:25 -0700

    initial commit

---
+++ fcc/js-problems/square-list.js
@@ -0,0 +1,12 @@
+/* We have defined a function named squareList. You need to complete the code for the squareList function using any combination of map(), filter(), and reduce() so that it returns a new array containing only the square of only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it. An example of an array containing only real numbers is [-3, 4.8, 5, 3, -3.2]. */
+
+const squareList = (arr) => {
+  // Only change code below this line
+  
+  return arr.filter(num => (num > 0 && num % 1 === 0)).map(num => num * num);
+  
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
