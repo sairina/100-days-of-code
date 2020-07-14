781210e9935a3ec8519a53578e9c860e04621d06
sairina <sairina.merinotsui@gmail.com>
2020-07-13 22:19:03 -0700

    initial commit

---
+++ fcc/js-problems/url-slug.js
@@ -0,0 +1,9 @@
+// The global variable
+var globalTitle = "Winter Is Coming";
+
+// Only change code below this line
+function urlSlug(title) {
+  return title.split(/\W/).filter(word => word !== '').join('-').toLowerCase();
+
+}
+// Only change code above this line
