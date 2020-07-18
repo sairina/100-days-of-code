8c2d4f83a142f5ce2fa6ddabd55a770f9334f643
sairina <sairina.merinotsui@gmail.com>
2020-07-17 21:33:21 -0700

    shorter refactor

--- fcc/js-problems/search-and-replace.js
+++ fcc/js-problems/search-and-replace.js
@@ -11,17 +11,29 @@ Note
 Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
 */
 
+// function myReplace(str, before, after) {
+//   let arr = str.split(' ');
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] === before) {
+//       if (before[0] === before[0].toUpperCase()){
+//         after = after[0].toUpperCase() + after.slice(1);
+//       }
+//       arr[i] = after;
+//     }
+//   }
+//   return arr.join(' ');
+// }
+
 function myReplace(str, before, after) {
-  let arr = str.split(' ');
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === before) {
-      if (before[0] === before[0].toUpperCase()){
-        after = after[0].toUpperCase() + after.slice(1);
-      }
-      arr[i] = after;
-    }
+  let index = str.indexOf(before);
+
+  if (str[index] === str[index].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
   }
-  return arr.join(' ');
+
+  str = str.replace(before, after);
+
+  return str;
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
