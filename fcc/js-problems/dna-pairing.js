3a5e4741fd72b42f9e5c14721c3d0012cddf2834
sairina <sairina.merinotsui@gmail.com>
2020-07-17 21:45:51 -0700

    refactor

--- fcc/js-problems/dna-pairing.js
+++ fcc/js-problems/dna-pairing.js
@@ -10,21 +10,35 @@ For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
 The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 */
 
-function pairElement(str) {
-  let arr = [];
+// function pairElement(str) {
+//   let arr = [];
   
-  for (let base of str) {
-    if (base === 'G') {
-      arr.push(['G', 'C']);
-    } else if (base === 'C') {
-      arr.push(['C', 'G']);
-    } else if (base === 'A') {
-      arr.push(['A', 'T']);
-    } else {
-      arr.push(['T', 'A']);
-    }
+//   for (let base of str) {
+//     if (base === 'G') {
+//       arr.push(['G', 'C']);
+//     } else if (base === 'C') {
+//       arr.push(['C', 'G']);
+//     } else if (base === 'A') {
+//       arr.push(['A', 'T']);
+//     } else {
+//       arr.push(['T', 'A']);
+//     }
+//   }
+//   return arr;
+// }
+
+// pairElement("GCG");
+
+function pairElement(str) {
+  let basePairs = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C'
   }
-  return arr;
+
+  let arr = str.split('');
+  return arr.map(base => [base, basePairs[base]]);
 }
 
 pairElement("GCG");
