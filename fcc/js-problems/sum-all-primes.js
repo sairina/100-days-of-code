504bfe6d321c16361f090fb4bf82b5abf65f9b12
sairina <sairina.merinotsui@gmail.com>
2020-07-19 23:16:18 -0700

    initial commit

---
+++ fcc/js-problems/sum-all-primes.js
@@ -0,0 +1,28 @@
+/*
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+*/
+
+function sumPrimes(num) {
+  let sum = 2;
+  
+  for (let i = 3; i <= num; i++){
+    if(isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+sumPrimes(10);
+//2, 3, 5, 7
