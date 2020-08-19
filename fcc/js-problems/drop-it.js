e06e0edfc812fc71bc1bb6f89cb9379623ba41ae
sairina <sairina.merinotsui@gmail.com>
2020-08-18 21:51:13 -0700

    initial commit

---
+++ fcc/js-problems/drop-it.js
@@ -0,0 +1,19 @@
+/*
+
+Intermediate Algorithm Scripting: Drop it
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+*/
+
+function dropElements(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr.slice(i);
+    }
+  }
+  return [];
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
